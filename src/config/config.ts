4f5637b5af14a89f623049a74cd10e7cc0128b3e
Divine Potential <divineckpotential@gmail.com>
2023-02-01 14:45:30 +0100

    first commit

---
+++ src/config/config.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const MONGO_USERNAME = process.env.MONGO_USERNAME || 'root';
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || 'root';
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.8rtet.mongodb.net/?retryWrites=true&w=majority`;
+
+const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 1337;
+
+export const config = {
+    mongo: {
+       url: MONGO_URL
+    },
+
+    server: {
+        port: SERVER_PORT
+    }
+};
+
